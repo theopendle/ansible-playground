e2e715bf71a6f26525ce56ff72f14ce124640ac3
Theo Pendle <theopendle@gmail.com>
2022-04-01 12:31:14 +0200

    feat: retain only target container

--- ansible-controller/run_playbook.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sshpass -p ansible ssh-copy-id -o "StrictHostKeyChecking no" -i $HOME/.ssh/id_rsa ansible@ansible-target
-
-command="ansible-playbook ${@}"
-
-echo "Executing ${command}"
-
-bash -c "${command}"
